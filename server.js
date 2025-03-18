8d795a45f92571b1210ee039e626c484c27abaa9
emilyaumala <emilyaumala1@gmail.com>
2025-03-17 19:19:49 -0500

    Update server.js

--- server.js
+++ server.js
@@ -161,7 +161,7 @@ app.post("/guardar", async (req, res) => {
 
         // Crear y guardar la oportunidad
         const oportunidad = new Oportunidad({
-            nombreProyecto: nuevoProyecto._id,
+            nombreProyecto: req.body.nombreProyecto,
             montoEstimado: req.body.montoEstimado,
             faseVenta: req.body.faseVenta.faseVenta,
             fechaInicio: req.body.fechaInicio,
