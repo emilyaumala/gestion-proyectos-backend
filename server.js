998c98712b8b3c02e43e8002f3d5f5e5fc337f0d
emilyaumala <emilyaumala1@gmail.com>
2025-03-19 10:49:59 -0500

    Update server.js

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ const Cliente = mongoose.model("Cliente", clienteSchema, "Cliente");
 const proyectoSchema = new mongoose.Schema({
     cliente: { type: mongoose.Schema.Types.ObjectId, ref: "Cliente", required: true },
     nombreProyecto: { type: String, required: true },
+      proyectoId: { type: mongoose.Schema.Types.ObjectId, ref: "Proyecto", required: true },
     area: { type: mongoose.Schema.Types.ObjectId, ref: "Area", required: true },
     montoEstimado: { type: Number, required: true },
     faseVenta: { type: mongoose.Schema.Types.ObjectId, ref: "FaseVenta", required: true },
