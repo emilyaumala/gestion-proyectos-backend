2fed85bddb2b495eea08dd9522baf6226204a136
emilyaumala <emilyaumala1@gmail.com>
2025-03-20 08:09:29 -0500

    Update server.js

--- server.js
+++ server.js
@@ -306,10 +306,13 @@ app.get("/informeOportunidad/:idProyecto", async (req, res) => {
     const respComercial = proyecto.respComercial?.respComercial || "Responsable comercial no disponible";
     const respTecnico = proyecto.respTecnico?.respTecnico || "Responsable técnico no disponible";
     const cliente = proyecto.cliente?.cliente || "Cliente no disponible";
-    const oportunidadesConLapso = oportunidades.map((oportunidad) => ({
-      ...oportunidad.toObject(),
-  lapsoEjecucion: `${oportunidad.cantidadLapso} ${oportunidad.unidadLapso}` || "Lapso no disponible"})
-console.log('unidadLapso:', proyecto.unidadLapso);
+const oportunidadesConLapso = oportunidades.map((oportunidad) => ({
+  ...oportunidad.toObject(),
+  lapsoEjecucion: (oportunidad.cantidadLapso && oportunidad.unidadLapso)
+    ? `${oportunidad.cantidadLapso} ${oportunidad.unidadLapso}`
+    : "Lapso no disponible"
+}));
+
 
 
     // 3. Buscar oportunidades por proyectoId (no por nombreProyecto)
